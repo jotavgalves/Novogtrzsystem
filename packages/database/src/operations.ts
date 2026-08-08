f139568cf131ed7baef548890b8c55a6d7bd85a8
jotavgalves <jvgacontato@gmail.com>
2026-08-08 10:20:18 -0300

    feat: expoe configuracao e dados de recibos

--- packages/database/src/operations.ts
+++ packages/database/src/operations.ts
@@ -2,6 +2,7 @@ export * from './operation-cancellation';
 export * from './operation-checkout';
 export * from './operation-core';
 export * from './operation-items';
+export * from './operation-receipts';
 export * from './operation-service-point-deletion';
 export * from './operation-vouchers';
 export * from './service-point-pins';
